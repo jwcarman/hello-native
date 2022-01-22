652556ffedeb8220c1378187f5d2261f8e7670b5
James Carman <jcarman@callibrity.com>
2022-01-22 18:55:41 -0500

    Adding hello endpoint

---
+++ src/test/java/com/callibrity/spring/hello/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.callibrity.spring.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+// ------------------------------ FIELDS ------------------------------
+
+    @Autowired
+    private MockMvc mockMvc;
+
+// -------------------------- OTHER METHODS --------------------------
+
+    @Test
+    void shouldSayHello() throws Exception {
+        mockMvc.perform(get("/hello/World"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+}
